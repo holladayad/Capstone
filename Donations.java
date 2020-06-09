05b8414177637584c7785d5961f2bf1dc85c25ff
mashzaman <64058615+mashzaman@users.noreply.github.com>
2020-06-09 13:05:38 -0400

    Update Donations.java

--- Donations.java
+++ Donations.java
@@ -1,6 +1,11 @@
 package CIS484.Capstone;
 
 
-public class Donations {
+public class Donations 
+{
+    private int donationID;
+    private static int nextDonationID = 0;
+    private ArrayList<Item> donationItems = new ArrayList<>();
+    
     
 }
