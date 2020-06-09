896e3985ac09e5a676ed54f0c7b6ba08f11cbe23
mashzaman <64058615+mashzaman@users.noreply.github.com>
2020-06-09 13:17:48 -0400

    Update Donations.java

--- Donations.java
+++ Donations.java
@@ -7,5 +7,29 @@ public class Donations
     private static int nextDonationID = 0;
     private ArrayList<Item> donationItems = new ArrayList<>();
     
+    public Donation (String firstName, String lastName, String city, String state, String zipcode, String phoneNumber, String email)
+    {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.state = state;
+        this.zipCode = zipCode;
+        this.phoneNumber = phoneNumber;
+        this.email = email;
+        this.donationID = nextDonationID++;
+    }
+    
+    public void amountSold(Items donationItem)
+    {
+        
+    }
+    public void getProfit()
+    {
+        
+    }
+    public void getQuantity()
+    {
+        
+    }
+
     
 }
